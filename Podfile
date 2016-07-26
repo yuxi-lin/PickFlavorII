3cedca2546170303c9b4df82bc32e48f8afb5d59
Yuxi Lin <yuxi.lin@mobileforming.com>
2016-07-26 10:23:49 -0700

    Initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'PickFlavorII' do
+  pod 'Alamofire', '~> 2.0'
+  pod 'MBProgressHUD', '~> 0.9.0'
+end
---
+++ Pods/Target Support Files/Pods-PickFlavorII/Pods-PickFlavorII-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PickFlavorII : NSObject
+@end
+@implementation PodsDummy_Pods_PickFlavorII
+@end
