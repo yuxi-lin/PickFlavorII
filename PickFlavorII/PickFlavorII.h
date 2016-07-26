f53fded7ad90af0aac6616631dd3411b7482ee31
Yuxi Lin <yuxi.lin@mobileforming.com>
2016-07-26 09:47:20 -0700

    Initial Commit

---
+++ PickFlavorII/PickFlavorII.h
@@ -0,0 +1,19 @@
+//
+//  PickFlavorII.h
+//  PickFlavorII
+//
+//  Created by Yuxi Lin on 7/26/16.
+//  Copyright © 2016 Yuxi Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PickFlavorII.
+FOUNDATION_EXPORT double PickFlavorIIVersionNumber;
+
+//! Project version string for PickFlavorII.
+FOUNDATION_EXPORT const unsigned char PickFlavorIIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickFlavorII/PublicHeader.h>
+
+
